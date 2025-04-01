6fe446b8e38984cd5262e206eaf1c1dc01e84651
damodara2006 <damodara2006@gmail.com>
2025-04-01 21:45:54 +0530

    refactor: enhance layout and styling in Park component for better user experience

--- frontend/src/Pages/Park.jsx
+++ frontend/src/Pages/Park.jsx
@@ -50,25 +50,28 @@ function Park() {
       
     }
   return (
-    <div className='w-[100%] h-screen flex justify-center items-center py-56 bg-gradient-to-r from-teal-400 to-yellow-200 z-0' >
+    <div className='w-[100%] h-screen flex justify-center py-56 bg-gradient-to-r from-teal-400 to-yellow-200 z-0 items-center' >
       <ToastContainer/>
       {
         occupied? 
       <p className=' text-[2400%]  text-red-400 z-20 ' >OCUUPIED</p> : ""
 
       }
-      <div className='p-34 border shadow-2xl rounded-2xl  absolute z-100 bg-gradient-to-r to-teal-400 from-yellow-200' >
-      <div className="flex flex-col h-[100%] font-roboto items-center  justify-evenly  gap-y-10 z-30 blur-none"  >
+      <div className='w-full h-full flex justify-center items-center'>
+      <div className='  w-[70%] h-[75%] shadow-2xl  flex-wrap rounded-2xl  absolute z-100 bg-gradient-to-r to-teal-400 from-yellow-200' >
+      <div className="flex flex-col h-[100%] font-roboto items-center  justify-evenly  gap-y-10 z-30 blur-none px-[0%] py-[5%]"  >
         {/* <label htmlFor="number" className='text-5xl w-fit  h-fit'>parkingNumber</label> */}
-        <input type='number' id='number' className='border outline-0  rounded-full h-14  w-96 pl-5  focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out text-center' value={number} placeholder="Enter the parking number" onChange={e=>setparkingNumber(e.target.value)}/>
-        <input type="text" className="border  outline-0 rounded-full h-14  w-96 pl-5 focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out" placeholder="Enter the username" onChange={e=>setusername(e.target.value)}/>
-        <input type="text"className='border outline-0 rounded-full h-14  w-96 pl-5 focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out '  placeholder="Enter carnumber" onChange={e=>setcarnumber(e.target.value)}/>
-        <input type="text"className='border outline-0 rounded-full h-14  w-96 pl-5 focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out' placeholder="Enter phone number" onChange={e=>setuserphone(e.target.value)}/>
-        <input type="file"className='border outline-0 rounded-full h-14  w-96 pl-5 focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out' onChange={e=>setcarpicture(e.target.files)} />
-        <input type="date" className='border outline-0 rounded-full h-14  w-96 pl-5 focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out' onChange={(e)=>{setdate(e.target.value)}}/>
+        <input type='number' id='number' className='border outline-0  rounded-full h-[7%]  w-[63%]  text-[75%] focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out text-center' value={number} placeholder="Enter the parking number" onChange={e=>setparkingNumber(e.target.value)}/>
+        <input type="text" className="border  outline-0 rounded-full h-[8%]  w-[63%] pl-[5%]  text-[75%] placeholder:text-[100%]  focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out" placeholder="Enter the username" onChange={e=>setusername(e.target.value)}/>
+        <input type="text"className='border outline-0 rounded-full h-[8%]  w-[63%] pl-[5%]  text-[75%] placeholder:text-[100%] focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out '  placeholder="Enter carnumber" onChange={e=>setcarnumber(e.target.value)}/>
+        <input type="text"className='border outline-0 rounded-full h-[8%]  w-[63%] pl-[5%]  text-[75%] placeholder:text-[100%] focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out' placeholder="Enter phone number" onChange={e=>setuserphone(e.target.value)}/>
+        <input type="file"className='border outline-0 rounded-full h-[8%]  w-[63%] pl-[5%]  text-[75%] placeholder:text-[100%] focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out' onChange={e=>setcarpicture(e.target.files)} />
+        <input type="date" className='border outline-0 rounded-full h-[8%]  w-[63%] pl-[5%]  text-[75%] placeholder:text-[100%] focus:bg-gradient-to-r from-orange-200 focus:transition-all focus:duration-300 ease-in-out' onChange={(e)=>{setdate(e.target.value)}}/>
       <button className='border px-5 py-2 hover:bg-white transition-all rounded-md' onClick={handlesubmit} >Submit</button>
       </div>
       </div>
+      </div>
+     
       
       {/* <button onClick={handlesubmit} >Submit</button> */}
     </div>
